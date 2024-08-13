c986b3916a7c0f891d32c2fe78553235a535fc41
Acchu <acchu1915@gmail.com>
2024-08-14 02:07:31 +0530

    Fix attempt for Repl issue Modified file path to be relative

--- node_Informational_Site.js
+++ node_Informational_Site.js
@@ -9,25 +9,26 @@ const server = http.createServer((req, res) =>
 
 server.listen(8080);
 
+
+
 async function whatToSend(req, res)
 {
     let contentToSend;
-    let filepath = '/home/odinstudent/OdinProject/Node/Odin_Basic_Informational_Site/'
     if (req.url === '/' || req.url === '/index')
     {
-        contentToSend = await fs.readFile(`${filepath}index.html`, { encoding: 'utf-8' })
+        contentToSend = await fs.readFile('index.html', { encoding: 'utf-8' })
     }
     else if (req.url === '/about')
     {
-        contentToSend = await fs.readFile(`${filepath}about.html`, { encoding: 'utf-8' })
+        contentToSend = await fs.readFile('about.html', { encoding: 'utf-8' })
     }
     else if (req.url === '/contact_me')
     {
-        contentToSend = await fs.readFile(`${filepath}contact_me.html`, { encoding: 'utf-8' })
+        contentToSend = await fs.readFile('contact_me.html', { encoding: 'utf-8' })
     }
     else
     {
-        contentToSend = await fs.readFile(`${filepath}404.html`, { encoding: 'utf-8' })
+        contentToSend = await fs.readFile('404.html', { encoding: 'utf-8' })
     }
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(contentToSend)
